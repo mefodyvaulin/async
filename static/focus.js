5a4483e926965da1db18bc4427c5b1d6e8f9ff59
Barmin_George <183339070+BarminGeorge@users.noreply.github.com>
2026-04-28 17:15:12 +0500

    run

--- static/focus.js
+++ static/focus.js
@@ -6,19 +6,33 @@ const API = {
 };
 
 function run() {
-    sendRequest(API.organizationList, (orgOgrns) => {
-        const ogrns = orgOgrns.join(",");
-        sendRequest(`${API.orgReqs}?ogrn=${ogrns}`, (requisites) => {
-            const orgsMap = reqsToMap(requisites);
-            sendRequest(`${API.analytics}?ogrn=${ogrns}`, (analytics) => {
-                addInOrgsMap(orgsMap, analytics, "analytics");
-                sendRequest(`${API.buhForms}?ogrn=${ogrns}`, (buh) => {
-                    addInOrgsMap(orgsMap, buh, "buhForms");
-                    render(orgsMap, orgOgrns);
-                });
-            });
-        });
-    });
+    const orgOgrns = await API.organizationList(orgOgrns);
+    const ogrns = orgOgrns.join(",");
+
+    const requisites = await sendRequest(`${API.orgReqs}?ogrn=${ogrns}`);
+    const orgsMap = reqsToMap(requisites);
+
+
+    const analytics = await sendRequest(`${API.analytics}?ogrn=${ogrns}`);
+    addInOrgsMap(orgsMap, analytics, "analytics");
+
+    const buh = await sendRequest(`${API.buhForms}?ogrn=${ogrns}`);
+    addInOrgsMap(orgsMap, buh, "buhForms");
+    render(orgsMap, orgOgrns);
+
+    // sendRequest(API.organizationList, (orgOgrns) => {
+    //     const ogrns = orgOgrns.join(",");
+    //     sendRequest(`${API.orgReqs}?ogrn=${ogrns}`, (requisites) => {
+    //         const orgsMap = reqsToMap(requisites);
+    //         sendRequest(`${API.analytics}?ogrn=${ogrns}`, (analytics) => {
+    //             addInOrgsMap(orgsMap, analytics, "analytics");
+    //             sendRequest(`${API.buhForms}?ogrn=${ogrns}`, (buh) => {
+    //                 addInOrgsMap(orgsMap, buh, "buhForms");
+    //                 render(orgsMap, orgOgrns);
+    //             });
+    //         });
+    //     });
+    // });
 }
 
 run();
